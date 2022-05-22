e095e84d5d15fefb21202edd44ff4bd1ed187da2
James Chambers <05jchambers@gmail.com>
2022-05-21 21:27:43 -0600

    Add multiarch support

--- start.sh
+++ start.sh
@@ -126,12 +126,7 @@ sed -i "/server-portv6=/c\server-portv6=$PortIPV6" /minecraft/server.properties
 
 # Start server
 echo "Starting Minecraft server..."
-CPUArch=$(uname -m)
-if [[ "$CPUArch" == *"x86_64"* ]]; then
-    BASH_CMD="LD_LIBRARY_PATH=/minecraft /minecraft/bedrock_server"
-else
-    BASH_CMD="qemu-x86_64-static LD_LIBRARY_PATH=/minecraft /minecraft/bedrock_server"
-fi
+BASH_CMD="LD_LIBRARY_PATH=/minecraft /minecraft/bedrock_server"
 if command -v gawk &> /dev/null; then
   BASH_CMD+=$' | gawk \'{ print strftime(\"[%Y-%m-%d %H:%M:%S]\"), $0 }\''
 else
