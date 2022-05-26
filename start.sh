02e29d04cb355ad73666277287db8ff9efc5ba4f
James Chambers <05jchambers@gmail.com>
2022-05-25 23:58:51 -0600

    Add version environment variable

--- start.sh
+++ start.sh
@@ -136,7 +136,6 @@ else
     if [ ! -z "$DownloadFile" ]; then
         unzip -o "downloads/$DownloadFile" -x "*server.properties*" "*permissions.json*" "*whitelist.json*" "*valid_known_packs.json*" "*allowlist.json*"
         Permissions=$(chmod u+x bedrock_server >/dev/null)
-    else
         echo "$DownloadFile" >version_installed.txt
     fi
 fi
