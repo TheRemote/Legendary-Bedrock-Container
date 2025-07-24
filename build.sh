136bb0c2d196b58d4c55602a997081dc62739c21
James Chambers <05jchambers@gmail.com>
2025-07-24 17:04:33 -0600

    Add build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,25 @@
-docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:amd64 -f amd64.Dockerfile .
-docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:arm64v8 -f arm64v8.Dockerfile .
-docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:armv7 -f armv7.Dockerfile .
-docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:ppc64le -f ppc64le.Dockerfile .
-docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:s390x -f s390x.Dockerfile .
-docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:pi5-16k -f pi5-16k.Dockerfile .
-docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:pi5 -f pi5.Dockerfile .
-docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:rk3588 -f rk3588.Dockerfile .
+docker build -t 05jchambers/legendary-bedrock-container:amd64 -f amd64.Dockerfile .
+docker push 05jchambers/legendary-bedrock-container:amd64
+
+docker build -t 05jchambers/legendary-bedrock-container:arm64v8 -f arm64v8.Dockerfile .
+docker push 05jchambers/legendary-bedrock-container:arm64v8
+
+docker build -t 05jchambers/legendary-bedrock-container:armv7 -f armv7.Dockerfile .
+docker push 05jchambers/legendary-bedrock-container:armv7
+
+docker build -t 05jchambers/legendary-bedrock-container:ppc64le -f ppc64le.Dockerfile .
+docker push 05jchambers/legendary-bedrock-container:ppc64le
+
+docker build -t 05jchambers/legendary-bedrock-container:s390x -f s390x.Dockerfile .
+docker push 05jchambers/legendary-bedrock-container:s390x
+
+docker build -t 05jchambers/legendary-bedrock-container:pi5-16k -f pi5-16k.Dockerfile .
+docker push 05jchambers/legendary-bedrock-container:pi5-16k
+
+docker build -t 05jchambers/legendary-bedrock-container:pi5 -f pi5.Dockerfile .
+docker push 05jchambers/legendary-bedrock-container:pi5
+
+docker build -t 05jchambers/legendary-bedrock-container:rk3588 -f rk3588.Dockerfile .
+docker push 05jchambers/legendary-bedrock-container:rk3588
 
 ./manifest-tool-linux-amd64 push from-spec multi-arch-manifest.yaml
