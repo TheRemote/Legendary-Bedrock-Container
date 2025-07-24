3fafd24a9f4e92f6dac70ccd60759642dd997557
James Chambers <05jchambers@gmail.com>
2025-07-24 14:22:28 -0600

    Add other architectures

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendar
 docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:armv7 -f armv7.Dockerfile .
 docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:ppc64le -f ppc64le.Dockerfile .
 docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:s390x -f s390x.Dockerfile .
+docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:pi5-16k -f pi5-16k.Dockerfile .
+docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:pi5 -f pi5.Dockerfile .
+docker buildx build --provenance=true --sbom=true --push -t 05jchambers/legendary-bedrock-container:rk3588 -f rk3588.Dockerfile .
 
 ./manifest-tool-linux-amd64 push from-spec multi-arch-manifest.yaml
